d14a959f3e5b4c2b721b4f1556dc856a23418e3c
kkt98 <sux1011@naver.com>
2022-10-17 19:32:28 +0900

    TodoListModel 생성

---
+++ app/src/main/java/com/kkt1019/todolist_pro/model/TodoListModel.kt
@@ -0,0 +1,12 @@
+package com.kkt1019.todolist_pro.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class TodoListModel(
+    @PrimaryKey(autoGenerate = true)
+    var id : Int =  0,
+    val check : Boolean,
+    val List : String
+)
